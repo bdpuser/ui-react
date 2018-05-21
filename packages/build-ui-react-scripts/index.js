960c60416d52feee0ca7f4c156b8adf0f79f443e
Jim Cummins <jimthedev@gmail.com>
2018-05-21 15:08:20 -0500

    feat: add build-ui-react-scripts, upgrade to react-scripts 2.0.0

---
+++ packages/build-ui-react-scripts/index.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const inquirer = require("inquirer");
+const { execSync } = require("child_process");
+
+(async function() {
+  // Determine which react scripts version we should use
+  const answers = await inquirer.prompt([
+    {
+      name: "reactScriptsVersion",
+      message:
+        "Which version of create-react-app's react-scripts do you want to build from? (provide a sha or branch)"
+    }
+  ]);
+
+  const gitRoot = execSync("git rev-parse --show-toplevel")
+    .toString()
+    .split("\n")[0];
+  const buildUiRoot = path.resolve(
+    gitRoot + "/packages/build-ui-react-scripts"
+  );
+
+  // Create a new directory to hold our build
+  await execSync(`mkdir build`);
+  console.log("Downloading react-scripts (this may take a while...)");
+  const lernaJson = require("../../lerna.json");
+  await execSync(
+    `
+    git clone https://github.com/facebook/create-react-app.git &&
+    cd create-react-app &&
+    git reset --hard 66cc7a903e91777e223780b898122a4dd3491d54 &&
+    cp -R ./packages/react-scripts ../ &&
+    cd .. &&
+    rm -rf create-react-app
+    cd .. &&
+    npx merge-dirs ./src/ ./build/react-scripts --overwrite &&
+    npx replace-in-file 'appPackage.browserslist = defaultBrowsers;' 'appPackage.browserslist = defaultBrowsers;appPackage.private = true;appPackage.scripts.start="HTTPS=true PORT=8100 REACT_APP_OPEN_TO_URL=https://dev.commerce.spscommerce.com/localhost react-scripts start";'  ./build/react-scripts/scripts/init.js
+    npx json -I -f ./build/react-scripts/package.json -e 'this.name="@spscommerce/ui-react-scripts"'
+    npx json -I -f ./build/react-scripts/package.json -e 'this.version="${
+      lernaJson.version
+    }"'
+    mv ./build/react-scripts ./build/ui-react-scripts 
+  `,
+    { cwd: path.resolve(buildUiRoot + "/build") }
+  );
+
+  console.log(
+    "Generated ui-react-scripts in ./build from ",
+    answers.reactScriptsVersion,
+    ". Please confirm that the scripts were generated properly before copying the contents of ./build into the ui-react-scripts package."
+  );
+})();
--- packages/ui-react-scripts/scripts/init.js
+++ packages/ui-react-scripts/scripts/init.js
@@ -5,19 +5,74 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-"use strict";
+'use strict';
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
-process.on("unhandledRejection", err => {
+process.on('unhandledRejection', err => {
   throw err;
 });
 
-const fs = require("fs-extra");
-const path = require("path");
-const chalk = require("chalk");
-const spawn = require("react-dev-utils/crossSpawn");
+const fs = require('fs-extra');
+const path = require('path');
+const chalk = require('chalk');
+const execSync = require('child_process').execSync;
+const spawn = require('react-dev-utils/crossSpawn');
+const { defaultBrowsers } = require('react-dev-utils/browsersHelper');
+const os = require('os');
+
+function isInGitRepository() {
+  try {
+    execSync('git rev-parse --is-inside-work-tree', { stdio: 'ignore' });
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+function isInMercurialRepository() {
+  try {
+    execSync('hg --cwd . root', { stdio: 'ignore' });
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+function tryGitInit(appPath) {
+  let didInit = false;
+  try {
+    execSync('git --version', { stdio: 'ignore' });
+    if (isInGitRepository() || isInMercurialRepository()) {
+      return false;
+    }
+
+    execSync('git init', { stdio: 'ignore' });
+    didInit = true;
+
+    execSync('git add -A', { stdio: 'ignore' });
+    execSync('git commit -m "Initial commit from Create React App"', {
+      stdio: 'ignore',
+    });
+    return true;
+  } catch (e) {
+    if (didInit) {
+      // If we successfully initialized but couldn't commit,
+      // maybe the commit author config is not set.
+      // In the future, we might supply our own committer
+      // like Ember CLI does, but for now, let's just
+      // remove the Git files to avoid a half-done state.
+      try {
+        // unlinkSync() doesn't work on directories.
+        fs.removeSync(path.join(appPath, '.git'));
+      } catch (removeErr) {
+        // Ignore.
+      }
+    }
+    return false;
+  }
+}
 
 module.exports = function(
   appPath,
@@ -26,44 +81,42 @@ module.exports = function(
   originalDirectory,
   template
 ) {
-  const ownPackageName = require(path.join(__dirname, "..", "package.json"))
+  const ownPackageName = require(path.join(__dirname, '..', 'package.json'))
     .name;
-  const ownPath = path.join(appPath, "node_modules", ownPackageName);
-  const appPackage = require(path.join(appPath, "package.json"));
-  const useYarn = fs.existsSync(path.join(appPath, "yarn.lock"));
+  const ownPath = path.join(appPath, 'node_modules', ownPackageName);
+  const appPackage = require(path.join(appPath, 'package.json'));
+  const useYarn = fs.existsSync(path.join(appPath, 'yarn.lock'));
 
   // Copy over some of the devDependencies
   appPackage.dependencies = appPackage.dependencies || {};
 
-  // Set private to true;
-  appPackage.private = true;
-
   // Setup the script rules
   appPackage.scripts = {
-    start:
-      "HTTPS=true PORT=8100 REACT_APP_OPEN_TO_URL=https://dev.commerce.spscommerce.com/localhost react-scripts start",
-    build: "react-scripts build",
-    test: "react-scripts test --env=jsdom",
-    eject: "react-scripts eject"
+    start: 'react-scripts start',
+    build: 'react-scripts build',
+    test: 'react-scripts test --env=jsdom',
+    eject: 'react-scripts eject',
   };
 
+  appPackage.browserslist = defaultBrowsers;appPackage.private = true;appPackage.scripts.start="HTTPS=true PORT=8100 REACT_APP_OPEN_TO_URL=https://dev.commerce.spscommerce.com/localhost react-scripts start";
+
   fs.writeFileSync(
-    path.join(appPath, "package.json"),
-    JSON.stringify(appPackage, null, 2)
+    path.join(appPath, 'package.json'),
+    JSON.stringify(appPackage, null, 2) + os.EOL
   );
 
-  const readmeExists = fs.existsSync(path.join(appPath, "README.md"));
+  const readmeExists = fs.existsSync(path.join(appPath, 'README.md'));
   if (readmeExists) {
     fs.renameSync(
-      path.join(appPath, "README.md"),
-      path.join(appPath, "README.old.md")
+      path.join(appPath, 'README.md'),
+      path.join(appPath, 'README.old.md')
     );
   }
 
   // Copy the files for the user
   const templatePath = template
     ? path.resolve(originalDirectory, template)
-    : path.join(ownPath, "template");
+    : path.join(ownPath, 'template');
   if (fs.existsSync(templatePath)) {
     fs.copySync(templatePath, appPath);
   } else {
@@ -75,40 +128,39 @@ module.exports = function(
 
   // Rename gitignore after the fact to prevent npm from renaming it to .npmignore
   // See: https://github.com/npm/npm/issues/1862
-  fs.move(
-    path.join(appPath, "gitignore"),
-    path.join(appPath, ".gitignore"),
-    [],
-    err => {
-      if (err) {
-        // Append if there's already a `.gitignore` file there
-        if (err.code === "EEXIST") {
-          const data = fs.readFileSync(path.join(appPath, "gitignore"));
-          fs.appendFileSync(path.join(appPath, ".gitignore"), data);
-          fs.unlinkSync(path.join(appPath, "gitignore"));
-        } else {
-          throw err;
-        }
-      }
+  try {
+    fs.moveSync(
+      path.join(appPath, 'gitignore'),
+      path.join(appPath, '.gitignore'),
+      []
+    );
+  } catch (err) {
+    // Append if there's already a `.gitignore` file there
+    if (err.code === 'EEXIST') {
+      const data = fs.readFileSync(path.join(appPath, 'gitignore'));
+      fs.appendFileSync(path.join(appPath, '.gitignore'), data);
+      fs.unlinkSync(path.join(appPath, 'gitignore'));
+    } else {
+      throw err;
     }
-  );
+  }
 
   let command;
   let args;
 
   if (useYarn) {
-    command = "yarnpkg";
-    args = ["add"];
+    command = 'yarnpkg';
+    args = ['add'];
   } else {
-    command = "npm";
-    args = ["install", "--save", verbose && "--verbose"].filter(e => e);
+    command = 'npm';
+    args = ['install', '--save', verbose && '--verbose'].filter(e => e);
   }
-  args.push("react", "react-dom");
+  args.push('react', 'react-dom');
 
   // Install additional template dependencies, if present
   const templateDependenciesPath = path.join(
     appPath,
-    ".template.dependencies.json"
+    '.template.dependencies.json'
   );
   if (fs.existsSync(templateDependenciesPath)) {
     const templateDependencies = require(templateDependenciesPath).dependencies;
@@ -127,37 +179,16 @@ module.exports = function(
     console.log(`Installing react and react-dom using ${command}...`);
     console.log();
 
-    const proc = spawn.sync(command, args, { stdio: "inherit" });
+    const proc = spawn.sync(command, args, { stdio: 'inherit' });
     if (proc.status !== 0) {
-      console.error(`\`${command} ${args.join(" ")}\` failed`);
+      console.error(`\`${command} ${args.join(' ')}\` failed`);
       return;
     }
   }
 
-  // Install react router, react-router dom, ui-react, etc
-  let spsTemplateCommand;
-  let spsTemplateArgs;
-
-  if (useYarn) {
-    spsTemplateCommand = "yarnpkg";
-    spsTemplateArgs = ["add"];
-  } else {
-    spsTemplateCommand = "npm";
-    spsTemplateArgs = ["install", "--save", verbose && "--verbose"].filter(
-      e => e
-    );
-  }
-  spsTemplateArgs.push(
-    "react-router",
-    "react-router-dom",
-    "@spscommerce/ui-react"
-  );
-  const reactRouterProc = spawn.sync(spsTemplateCommand, spsTemplateArgs, {
-    stdio: "inherit"
-  });
-  if (reactRouterProc.status !== 0) {
-    console.error(`\`${command} ${args.join(" ")}\` failed`);
-    return;
+  if (tryGitInit(appPath)) {
+    console.log();
+    console.log('Initialized a git repository.');
   }
 
   // Display the most elegant way to cd.
@@ -171,54 +202,54 @@ module.exports = function(
   }
 
   // Change displayed command to yarn instead of yarnpkg
-  const displayedCommand = useYarn ? "yarn" : "npm";
+  const displayedCommand = useYarn ? 'yarn' : 'npm';
 
   console.log();
   console.log(`Success! Created ${appName} at ${appPath}`);
-  console.log("Inside that directory, you can run several commands:");
+  console.log('Inside that directory, you can run several commands:');
   console.log();
   console.log(chalk.cyan(`  ${displayedCommand} start`));
-  console.log("    Starts the development server.");
+  console.log('    Starts the development server.');
   console.log();
   console.log(
-    chalk.cyan(`  ${displayedCommand} ${useYarn ? "" : "run "}build`)
+    chalk.cyan(`  ${displayedCommand} ${useYarn ? '' : 'run '}build`)
   );
-  console.log("    Bundles the app into static files for production.");
+  console.log('    Bundles the app into static files for production.');
   console.log();
   console.log(chalk.cyan(`  ${displayedCommand} test`));
-  console.log("    Starts the test runner.");
+  console.log('    Starts the test runner.');
   console.log();
   console.log(
-    chalk.cyan(`  ${displayedCommand} ${useYarn ? "" : "run "}eject`)
+    chalk.cyan(`  ${displayedCommand} ${useYarn ? '' : 'run '}eject`)
   );
   console.log(
-    "    Removes this tool and copies build dependencies, configuration files"
+    '    Removes this tool and copies build dependencies, configuration files'
   );
   console.log(
-    "    and scripts into the app directory. If you do this, you can’t go back!"
+    '    and scripts into the app directory. If you do this, you can’t go back!'
   );
   console.log();
-  console.log("We suggest that you begin by typing:");
+  console.log('We suggest that you begin by typing:');
   console.log();
-  console.log(chalk.cyan("  cd"), cdpath);
+  console.log(chalk.cyan('  cd'), cdpath);
   console.log(`  ${chalk.cyan(`${displayedCommand} start`)}`);
   if (readmeExists) {
     console.log();
     console.log(
       chalk.yellow(
-        "You had a `README.md` file, we renamed it to `README.old.md`"
+        'You had a `README.md` file, we renamed it to `README.old.md`'
       )
     );
   }
   console.log();
-  console.log("Happy hacking!");
+  console.log('Happy hacking!');
 };
 
 function isReactInstalled(appPackage) {
   const dependencies = appPackage.dependencies || {};
 
   return (
-    typeof dependencies.react !== "undefined" &&
-    typeof dependencies["react-dom"] !== "undefined"
+    typeof dependencies.react !== 'undefined' &&
+    typeof dependencies['react-dom'] !== 'undefined'
   );
 }
