ac93ade39c3f6b27aab5f21e78bd7877234e94ca
Jim Cummins <jim.for.cy@gmail.com>
2018-06-18 16:39:06 -0500

    feat: 🎸 release with debugging (#41)

--- packages/ui-react/src/tabbedNav/SpsTabbedNavItem.js
+++ packages/ui-react/src/tabbedNav/SpsTabbedNavItem.js
@@ -5,7 +5,6 @@ export class SpsTabbedNavItem extends Component {
   render() {
     // We want to pluck out the preset since
     // the tabbed-nav requires the "tabbed" preset
-    //
     const { preset, to, ...props } = this.props;
     return to ? (
       <SpsNavLinkButton {...props} to={to} preset="tabbed" />
